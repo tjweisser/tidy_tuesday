eff760a87c1ec529282b9a93a86a61543e6ad217
tjweisser <tillw@rocketmail.com>
2020-12-25 16:39:36 +0100

    add filter

--- so_survey/main.py
+++ so_survey/main.py
@@ -4,7 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %%
-df = pd.read_csv("survey_results_public.csv")
+df = pd.read_csv("survey_results_public.csv", index_col="Respondent")
+df.columns = [x.lower() for x in df.columns]
 df.head()
+df.info()
+
+# %%
+data = df.loc[:, "ethnicity"]
+
+plt.plot(data)
+# %%
+
 
 # %%
